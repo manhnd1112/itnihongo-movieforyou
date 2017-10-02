0afdcd6bc4660c5fc191c643759b9a2d2742f546
Tieupham <tieupham2803@gmail.com>
2017-10-02 16:37:39 +0700

    fix conflict

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
     get 'users/omniauth_callbacks'
-  resources :cities
+  # resources :cities
   get 'static_pages/home'
  resources :passwords
   devise_for :users
 
-  # root "static_pages#show"
-  root 'cities#index'
+  root "static_pages#show"
+  # root 'cities#index'
   devise_scope :user do  
   get "/users/auth/facebook/callback" => "users/omniauth_callbacks#facebook"
 end   
