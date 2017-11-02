186e8dc5b42607e9fd3622bfa25f59107975622f
VuThuHien <hienvu2401@gmail.com>
2017-11-02 12:22:01 +0700

    feedback + fix bug

--- app/controllers/feedbacks_controller.rb
+++ app/controllers/feedbacks_controller.rb
@@ -1,25 +1,16 @@
 class FeedbacksController < ApplicationController
-  def new; end
-
   def create
-    feedback = Feedback.new
-
-    return if feedback.update_attributes feedback_params
-    flash[:danger] = t "feedback.danger"
-  end
-
-  def update
-    feedback = find_feedback
-    update_feedback = feedback.update_attributes state: "canceled"
-    flash[:danger] = t "borrow.destroy.fail" unless update_feedback
+    @feedback = Feedback.new
+    if @feedback.update_attributes feedback_params
+      flash[:success] = t "flash.feedbacks.success"
+    else
+      flash[:danger] = t "flash.destroy_fail"
+    end
+    redirect_to contact_path
   end
 
   private
   def feedback_params
     params.require(:feedback).permit :content, :name, :subject, :email
-  end
-
-  def find_feedback
-    Feedback.find_by id: params[:id]
   end
 end
